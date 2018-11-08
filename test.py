fdba3adce6cddcb8dace02330460c765d84219ea
Steffen Schneider <steffenschneider@msn.com>
2018-11-08 13:03:50 +0100

    fix Burda

--- test.py
+++ test.py
@@ -5,19 +5,6 @@ from votingSchemes.Burda import Burda as BU
 from votingSchemes.AntiPlurality import AntiPlurality as AP
 import numpy
 
-
-def get_matrix(prefs):
-    candidates = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    output = []
-
-    for row in prefs.T.tolist():
-        temp = []
-        for index, _ in enumerate(row):
-            temp.append(row.index(Can(candidates[index])) + 1)
-        output.append(temp)
-    return numpy.array(output).T
-
-
 def main():
     # candidates
     a = Can('A')
@@ -33,7 +20,6 @@ def main():
         [b, d, e, c, a]
     ]).T
 
-    print(AP.get_scores(preferences, candidates))
 
 
 if __name__ == '__main__':
--- votingSchemes/Burda.py
+++ votingSchemes/Burda.py
@@ -6,7 +6,7 @@ class Burda(AbstractScheme):
 
     @staticmethod
     def get_score(preferences, candidates):
-        l = len(candidates)
-        factor = np.array(list(range(5, 0, -1))).reshape(l, 1)
+        length = len(candidates)
+        factor = np.array(list(range(length-1, -1, -1))).reshape(length, 1)
         return [((preferences == y).astype(int) * factor).sum() for y in candidates]
 
