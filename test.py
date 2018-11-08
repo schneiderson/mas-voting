3d33ec96860e8d4160d7832f356162e3007f4650
Omendra Manhar <omendra.manhar@gmail.com>
2018-11-08 11:16:36 +0100

    Refs #1 Converting prefs to matrix

--- test.py
+++ test.py
@@ -2,9 +2,10 @@ from Preference import Preference as Pref
 from Candidate import Candidate as Can
 from votingSchemes.PluralityVote import PluralityVote as PL
 from votingSchemes.Burda import Burda as BU
+import numpy
+
 
 def main():
-
     # candidates
     a = Can('a')
     b = Can('b')
@@ -12,11 +13,7 @@ def main():
     d = Can('d')
     e = Can('e')
 
-    p1 = Pref([a, b, c, d, e])
-    p2 = Pref([b, a, d, e, c])
-    p3 = Pref([e, d, b, c, a])
-    p4 = Pref([b, d, e, c, a])
-    prefs = [p1, p2, p3, p4]
+    preferences = numpy.array([[a, b, c, d, e], [b, a, d, e, c], [e, d, b, c, a], [b, d, e, c, a]])
 
     print(prefs)
     print(PL.get_score(prefs))
