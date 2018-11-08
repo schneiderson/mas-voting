e602cd20401c500d3e5bc90df23eaa262a73a12f
Omendra Manhar <omendra.manhar@gmail.com>
2018-11-08 18:12:42 +0100

    Adding to Bury Class Update

--- tacticalVoting/Bury.py
+++ tacticalVoting/Bury.py
@@ -1,7 +1,3 @@
-from votingSchemes.PluralityVote import PluralityVote as PL
-from votingSchemes.Burda import Burda as BU
-from votingSchemes.AntiPlurality import AntiPlurality as AP
-from votingSchemes.VotingForTwo import VotingForTwo as VT
 import numpy as np
 from HappinessScore import HappinessScore as Hap
 
@@ -48,34 +44,7 @@ class Bury(object):
         return [pref]
 
     def get_outcome(self, preferences):
-        if self.scheme == "BU":
-            return self.get_borda(preferences)
-
-        if self.scheme == "AP":
-            return self.get_anti_plurality(preferences)
-
-        if self.scheme == "VT":
-            return self.get_voting_for_two(preferences)
-
-        if self.scheme == "PL":
-            return self.get_plurality(preferences)
-
-        return self.get_borda(preferences)
-
-    def get_borda(self, preferences):
-        return BU.get_scores(preferences, self.candidates)
-
-    def get_plurality(self, preferences):
-        return PL.get_scores(preferences, self.candidates)
-
-    def get_voting_for_two(self, preferences):
-        return VT.get_scores(preferences, self.candidates)
-
-    def get_anti_plurality(self, preferences):
-        return AP.get_scores(preferences, self.candidates)
-
-    def get_new_winner(self, temp_pref):
-        return self.candidates[np.argsort(BU.get_scores(temp_pref, self.candidates))[-1]]
+        return self.scheme.get_scores(preferences)
 
     def get_new_happiness(self, outcome, voter_id):
         return Hap.get_scores(outcome, self.candidates, self.preferences)[0][voter_id]
