e5e93721bb4c461bbe26d1c5a30325720f7e1cfc
Jacob Salam <jacob_salam@hotmail.com>
2018-11-15 15:47:53 +0100

    Got rid of all the print statements

--- tacticalVoting/Compromising.py
+++ tacticalVoting/Compromising.py
@@ -8,23 +8,12 @@ class Compromising:
     def get_voting(outcome, candidates, preferences, happiness, voting_scheme):
         (m, n) = preferences.shape
         winner = candidates[np.argmax(outcome)]
-
-        print(winner)
-
-        print(preferences)
-
         # get indices of dissatisfied voters
         indices_dissat = np.argwhere(happiness < m-2).flatten()
-
-        print(indices_dissat)
-
-        print(winner)
-
         preferences = preferences.T
         newPref = [0] * preferences.shape[0]
         outcomeList = [0] * preferences.shape[0]
         manipulations = []
-
 
         for x in indices_dissat:
             newPref[x] = preferences[0].tolist()
@@ -32,7 +21,6 @@ class Compromising:
             first_pref = preferences[x][0]
             score = Hap.get_scores(outcome, candidates, preferences.T)[0][x]
             outcomeList[x] = outcome
-            print()
             for i in range(1, winner_index):
                 if i == winner_index:
                     continue
@@ -43,7 +31,6 @@ class Compromising:
                 preferences[x][i] = preferences[x][0]
                 preferences[x][0] = first_pref
                 newHappyness = Hap.get_scores(new_outcome, candidates, preferences.T)
-                print(score, newHappyness[0][x])
                 if newHappyness[0][x] > score:
                     print("WARNING: Identified potential for bulletVoting for voter {}".format(x))
 
@@ -58,7 +45,4 @@ class Compromising:
                                         new_outcome
                                         )
                     manipulations.append(manipulation)
-                    print(manipulation)
-
-
         return 0
