35090d9722162f13e28f7492602552d54d310115
Jacob Salam <jacob_salam@hotmail.com>
2018-11-15 15:45:07 +0100

    Working Compromising

--- tacticalVoting/Compromising.py
+++ tacticalVoting/Compromising.py
@@ -1,5 +1,7 @@
 import numpy as np
 from HappinessScore import HappinessScore as Hap
+from tacticalVoting.Manipulation import Manipulation as Mani
+
 
 class Compromising:
     @staticmethod
@@ -20,12 +22,9 @@ class Compromising:
 
         preferences = preferences.T
         newPref = [0] * preferences.shape[0]
-        hapScore = [0] * preferences.shape[0]
         outcomeList = [0] * preferences.shape[0]
+        manipulations = []
 
-        changed = []
-        betterScore = False
-        riskcount = 0
 
         for x in indices_dissat:
             newPref[x] = preferences[0].tolist()
@@ -41,26 +40,25 @@ class Compromising:
                 preferences[x][i] = first_pref
                 new_outcome = voting_scheme.get_scores(preferences.T, candidates)
                 prefTemp = preferences[x].tolist()
-                print(preferences[x])
                 preferences[x][i] = preferences[x][0]
                 preferences[x][0] = first_pref
                 newHappyness = Hap.get_scores(new_outcome, candidates, preferences.T)
-                print(score, "original:",preferences[x], newHappyness[0][x], outcome, new_outcome)
+                print(score, newHappyness[0][x])
                 if newHappyness[0][x] > score:
-                    riskcount = riskcount + 1
-                    print(x, " location")
-                    changed = changed + [x]
-                    hapScore[x] = newHappyness[0][x]
-                    score = newHappyness[0][x]
-                    newPref[x] = prefTemp
-                    outcomeList[x] = new_outcome
-                    betterScore = True
+                    print("WARNING: Identified potential for bulletVoting for voter {}".format(x))
 
-        if betterScore:
-            for x in changed:
-                #print(riskcount/len(preferences))
-                print("Voter", x+1, "has a happyness score of", hapScore[x], "and uses the preference", newPref[x], "the new results are", outcomeList[x])
-
+                    manipulation = Mani("BulletVoting",
+                                        voting_scheme.get_name(),
+                                        x,
+                                        preferences[x],
+                                        prefTemp,
+                                        score,
+                                        newHappyness[0][x],
+                                        outcome,
+                                        new_outcome
+                                        )
+                    manipulations.append(manipulation)
+                    print(manipulation)
 
 
         return 0
