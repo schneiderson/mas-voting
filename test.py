0e71f02d0738ab12717df5825b5a9283eda7bbc8
Steffen Schneider <steffenschneider@msn.com>
2018-11-21 17:01:32 +0100

    add scoring

--- test.py
+++ test.py
@@ -22,11 +22,31 @@ def main():
         [d, a, b, c, e]
     ]).T
 
+    num_voters = preferences.shape[1]
+    num_candidates = preferences.shape[0]
+
     voting_machine = VoMa(preferences, candidates)
     manipulations = voting_machine.get_manipulations()
 
+    mani_per_vs = {}
+
     for m in manipulations:
-        print(m.strategy + ": " + m.voting_scheme + ", Voter " + str(m.voter))
+        # print(m.strategy + ": " + m.voting_scheme + ", Voter " + str(m.voter))
+        if mani_per_vs.keys().__contains__(m.voting_scheme):
+            mani_per_vs[m.voting_scheme] += 1
+        else:
+            mani_per_vs[m.voting_scheme] = 1
+
+
+    num_all_mani = len(manipulations)
+
+    print("overall manipulations: {}".format(num_all_mani))
+    for mani, test in enumerate(mani_per_vs.keys()):
+        print("------------")
+        print("manipulations for {}: {}".format(test, mani_per_vs[test]))
+        print("score: {}".format(mani_per_vs[test]/num_voters))
+
+
 
 
 if __name__ == '__main__':
